fd2677094484969262d02acc63e93b9d12fc836a
Joel Mathew <joelmathewpalamattom@gmail.com>
2025-07-10 21:06:55 +0530

    Add files via upload

--- bank.py
+++ bank.py
@@ -35,7 +35,7 @@ class Bank:
 
 
     def chk(self):
-        pwd = input("Enter your Bank Password: ")
+        pwd = input("Enter your Login Password: ")
         if pwd == self._pwd:
             return True
         else:
@@ -104,24 +104,139 @@ class Bank:
         else:
             print("\nIncorrect Password")
 
-
+def wr():
+    print("Wrong password!")
 if __name__ == "__main__":
     b = Bank()
-    while True:
-        choice = show()
-        match choice:
-            case '1':
-                b.show()
-            case '2':
-                b.deposit(input("Enter amount to deposit: $"))
-            case '3':
-                b.withdraw(input("Enter amount to withdraw: $"))
-            case '4':
-                b.passwd()
-            case '5':
-                break
-            case _:
-                print("Invalid choice!")
-    star()
-    print("  ***   HAVE A GOOD DAY!   ***  ")
-    star()
+    with open("limit.txt", "r") as file:
+        val = int(file.read())
+    if  val <= 3 and val > -1:
+        if b.chk():
+            while True:
+                choice = show()
+                match choice:
+                    case '1':
+                        if b.chk():
+                            b.show()
+                        else:
+                            wr()
+                            quit()
+                    case '2':
+                        if b.chk():
+                            b.deposit(input("Enter the amount to deposit: $"))
+                        else:
+                            wr()
+                            quit()
+                    case '3':
+                        if b.chk():
+                            b.withdraw(input("Enter the amount to withdraw: $"))
+                        else:
+                            wr()
+                            quit()
+                    case '4':
+                        b.passwd()
+                    case '5':
+                        break
+                    case _:
+                        print("Invalid choice!")
+            star()
+            print("  ***   HAVE A GOOD DAY!   ***  ")
+            star()
+        else:
+            wr()
+            with open("limit.txt", "r+") as t:
+                cont = int(t.read())
+                cont += 1
+                print(f"{4 - cont} attempts left.")
+                t.seek(0)
+                t.write(str(cont))
+            quit()
+
+    else:
+        print("Limit reached! Please try again after 48 hours.")
+        quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     def show(balance):
+#         print(f"Balance: ${balance:.2f}")
+
+# def dep():
+#     try:
+#         amount = float(input("Enter the amount you want to deposit: $"))
+#         if amount < 0:
+#             print("Amount can't be negative!")
+#             return 0
+#         else:
+#             star()
+#             print(f"${amount} has been debited to your account.")
+#             star()
+#             return amount
+#     except:
+#         print("Texts not allowed! Please try again!")
+#         return 0
+
+# def wdraw(balance):
+#     try:
+#         amount = float(input("Enter the amount you want to withdraw: $"))
+#         if amount < 0:
+#             print("Amount can't be negative!")
+#             return 0
+#         elif amount > balance:
+#             print("Insufficient funds!")
+#             return 0
+#         else:
+#             star()
+#             print(f"${amount} has been withdrawn from your account.")
+#             star()
+#             return amount
+#     except:
+#         print("Text not allowed! Please try again!")
+#         return 0
+
+# def prog():
+#     balance = 0
+#     while True:
+#         star()
+#         print("""      WELCOME TO ONLINE BANKING
+#         1. Show Balance.
+#         2. Deposit Amount.
+#         3. Withdraw Amount.
+#         4. Exit.""")
+#         star()
+#         choice = input("Select your desired option (1 - 4): ")
+#         match choice:
+#             case '1':
+#                 show(balance)
+#             case '2':
+#                 balance += dep()
+#             case '3':
+#                 balance -= wdraw(balance)
+#             case '4':
+#                 break
+#             case _ :
+#                 print("Invalid choice!")
+#     star()
+#     print("      THANK YOU USING OUR SERVICE      ")
+#     star()
+
+
+
+# if __name__ == '__main__':
+#     prog()
